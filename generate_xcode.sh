8df6282d506a13e74e794949b0779a0e994710cc
Dmitry Baidaev <d.baidaev@corp.mail.ru>
2019-10-26 15:13:20 +0300

    + generate_xcode.sh

--- generate_xcode.sh
+++ generate_xcode.sh
@@ -1,3 +1,17 @@
-mkdir xcode-build
+#!/bin/bash
+
+PLATFORM=macos
+
+BUILD_TYPE=Release
+BUILD_DIR=build-lib-$PLATFORM
+
+FLAGS_BUILD_TYPE="-DCMAKE_BUILD_TYPE=${BUILD_TYPE}"
+CMAKE_PLATFORM_FLAGS="${FLAGS_BUILD_TYPE} -DCMAKE_TOOLCHAIN_FILE=../cmake-config/toolchain.cmake -DPLATFORM=MACOS64"
+
+
+if [ ! -d "$2" ] ; then
+    mkdir xcode-build
+fi
+
 cd xcode-build
-cmake -G Xcode ..
+cmake $CMAKE_PLATFORM_FLAGS -DAS_LIBRARY:BOOL=YES -G Xcode ..
